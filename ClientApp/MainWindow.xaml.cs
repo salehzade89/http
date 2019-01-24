f948ea04154f36a198a5a5647efb92d62413745b
Oleksandr Malomed <malomed@itstep.org>
2019-01-24 22:08:40 +0400

    initial

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Windows;
+
+namespace ClientApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e) {
+            var client = new HttpClient();
+            var url = "http://localhost:5000/developers/";
+            var data = JsonConvert.SerializeObject(new {
+                Name = "John",
+                Message = this.msg.Text
+            });
+            var content = new StringContent(data);
+            var res = await client.PostAsync(url, content);
+            var str = await res.Content.ReadAsStringAsync();
+            dynamic msg = JsonConvert.DeserializeObject(str);
+            var sc = msg.StatusCode;
+            var r = msg.Result;
+            var op = msg.OperationTime;
+            MessageBox.Show($"{sc} {r} {op}");
+        }
+    }
+}
---
+++ HttpServer/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace HttpServer {
+    internal class Program {
+        private static void Main(string[] args) {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:5000/");
+            listener.Start();
+
+            while (true) {
+                var context = listener.GetContext();
+                var output = context.Response.OutputStream;
+                var writer = new StreamWriter(output);
+                var req = context.Request;
+                object data = "No data";
+                if (req.Url.Segments[1].Contains("developers")) {
+                    data = new string[] {
+                        "Shafag", "Zulfugar",
+                        "Javid", "Oqtay", "Alim"
+                    };
+                } else if (req.Url.Segments[1].Contains("designers")) {
+                    if (req.Url.Segments[2].Contains("fake")) {
+                        data = new string[] {
+                            "Sasha"
+                        };
+                    } else if (req.Url.Segments[2].Contains("actual")) {
+                        data = new string[] {
+                            "Elkhan", "Isa", "Nadir"
+                        };
+                    }
+
+                }
+                writer.WriteLine(JsonConvert.SerializeObject(data));
+                writer.Close();
+                context.Response.StatusCode = 200;
+                context.Response.Close();
+
+
+
+
+
+
+                //var context = listener.GetContext();
+                //if (context.Request.HttpMethod.ToUpper() != "POST") {
+                //    Console.WriteLine("not post");
+                //    context.Response.StatusCode = 404;
+                //    context.Response.Close();
+                //    continue;
+                //}
+                //if (context.Request.ContentLength64 == 0) {
+                //    Console.WriteLine("no data");
+                //    context.Response.StatusCode = 403;
+                //    context.Response.Close();
+                //    continue;
+                //}
+                //var stream = context.Request.InputStream;
+                //var reader = new StreamReader(stream);
+                //var str = reader.ReadLine();
+                //var msg = JsonConvert.DeserializeObject<MessageItem>(str);
+                //if (msg == null) {
+                //    Console.WriteLine("invalid data");
+                //    context.Response.StatusCode = 403;
+                //    context.Response.Close();
+                //    continue;
+                //}
+                //Console.WriteLine($"{msg.Name}: {msg.Message}");
+
+                //var output = context.Response.OutputStream;
+                //var writer = new StreamWriter(output);
+                //var data = new {
+                //    Result = "MessageProcessed",
+                //    StatusCode = 200,
+                //    OperationTime = DateTime.Now
+                //};
+                //writer.WriteLine(JsonConvert.SerializeObject(data));
+                //writer.Close();
+                //context.Response.StatusCode = 200;
+                //context.Response.Close();
+            }
+        }
+    }
+}
